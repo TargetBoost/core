3587389c91b9b77678ac486ad85432e1329b680a
AndreySHSH <laptev.andrey@icloud.com>
2022-09-20 01:07:22 +0300

    fix

--- internal/transport/http/handler/user.go
+++ internal/transport/http/handler/user.go
@@ -38,7 +38,7 @@ func (h *Handler) CreateUser(ctx iris.Context) {
 		ctx.StatusCode(400)
 		_ = ctx.JSON(iris.Map{
 			"status": iris.Map{
-				"message": err,
+				"message": err.Error(),
 			},
 		})
 		return
