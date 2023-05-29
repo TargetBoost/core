1eb2f3434b83a3346f3abfe83187f9f71eec3ee2
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-29 22:47:29 +0300

    fix time

--- internal/repositories/target/target.go
+++ internal/repositories/target/target.go
@@ -90,7 +90,7 @@ func (r *Repository) UpdateTaskStatus(q models.Queue) {
 }
 
 func (r *Repository) UpdateTask(q models.Queue) {
-	r.db.Table("queues").UpdateColumns(&q)
+	r.db.Table("queues").UpdateColumns(&q).Where("id = ?", q.ID)
 }
 
 func (r *Repository) GetTaskDISTINCT() []models.Queue {
