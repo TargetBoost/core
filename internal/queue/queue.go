e6b824295c565a2299618e8addc3f98d09e15b30
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-29 23:17:29 +0300

    fix time

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const timeChange = 20 * time.Second
+const timeChange = time.Second * 20
 
 type Queue struct {
 	Line        chan []Task
@@ -55,9 +55,9 @@ func (q Queue) AppointTask() {
 					continue
 				}
 
-				logger.Info(v.UpdatedAt.Sub(time.Now()).Seconds(), timeChange.Seconds())
+				if v.UpdatedAt.After(time.Now().Add(timeChange)) {
+					logger.Info(v.UpdatedAt.Sub(time.Now()).Seconds(), timeChange.Seconds())
 
-				if v.UpdatedAt.Sub(time.Now()).Seconds() > timeChange.Seconds() {
 					v.UID = 0
 					v.UpdatedAt = time.Now()
 					v.Status = 0
