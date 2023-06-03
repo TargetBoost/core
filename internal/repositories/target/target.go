0d3942eaba5be491cf9308c4edc12c359dca05ea
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-06-03 20:55:21 +0300

    get photo link

--- internal/repositories/target/target.go
+++ internal/repositories/target/target.go
@@ -30,7 +30,7 @@ func (r *Repository) GetTargets(uid uint) []models.Target {
 		v.Count = sc.Count
 
 		var cmc models.ChatMembersChanel
-		chName := strings.Split("/", v.Link)[len(strings.Split("/", v.Link))-1]
+		chName := strings.Split("/", v.Link)[len(strings.Split("/", v.Link))]
 
 		r.db.Debug().Table("chat_members_chanels").Where("user_name = ?", strings.ToLower(chName)).Find(&cmc)
 
