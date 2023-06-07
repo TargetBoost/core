2def21fc4b51b4acd505beea2654026baaf41384
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-06-08 02:21:59 +0300

    fix token

--- internal/transport/http/handler/autentification.go
+++ internal/transport/http/handler/autentification.go
@@ -4,13 +4,15 @@ import (
 	"github.com/kataras/iris/v12"
 )
 
+const errorAuth = "Ошибка авторизации"
+
 func (h *Handler) IsAuth(ctx iris.Context) {
 	rawToken := ctx.GetHeader("Authorization")
 	if len(rawToken) == 0 {
 		ctx.StatusCode(401)
 		_ = ctx.JSON(iris.Map{
 			"status": iris.Map{
-				"message": "Not token required",
+				"message": errorAuth,
 			},
 			"data": nil,
 		})
@@ -22,7 +24,7 @@ func (h *Handler) IsAuth(ctx iris.Context) {
 		ctx.StatusCode(401)
 		_ = ctx.JSON(iris.Map{
 			"status": iris.Map{
-				"message": "Bad token required",
+				"message": errorAuth,
 			},
 			"data": nil,
 		})
